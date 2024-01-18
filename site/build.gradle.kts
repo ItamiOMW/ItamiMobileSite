06f85dc16c102410efb75520661d815bc03193b0
Вадим <itamiomw@gmail.com>
2024-01-18 10:11:38 +0300

    1) Removed Font Awesome icons; 2) Added Material Design icons

--- site/build.gradle.kts
+++ site/build.gradle.kts
@@ -35,7 +35,7 @@ kotlin {
             implementation(compose.html.core)
             implementation(libs.kobweb.core)
             implementation(libs.kobweb.silk)
-            implementation(libs.silk.icons.fa)
+            implementation(libs.silk.icons.mdi)
             implementation(libs.kobwebx.markdown)
         }
     }
