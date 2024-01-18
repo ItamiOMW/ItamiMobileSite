2a74d7a9f541cad20509a2d6eb0659ef91fd003f
Вадим <itamiomw@gmail.com>
2024-01-18 10:12:57 +0300

    1) Removed Poppins Bold;

--- site/src/jsMain/kotlin/com/itami/itami_mobile/utils/Res.kt
+++ site/src/jsMain/kotlin/com/itami/itami_mobile/utils/Res.kt
@@ -5,16 +5,11 @@ object Res {
     object FontFamilies {
         const val POPPINS_REGULAR = "PoppinsRegular"
         const val POPPINS_MEDIUM = "PoppinsMedium"
-        const val POPPINS_BOLD = "PoppinsBold"
-    }
-
-    object Icons {
-        const val LOGO_DARK = "images/logo_dark.svg"
-        const val LOGO_LIGHT = "images/logo_light.svg"
     }
 
     object Images {
-
+        const val LOGO_DARK = "images/logo_dark.svg"
+        const val LOGO_LIGHT = "images/logo_light.svg"
     }
 
 }
