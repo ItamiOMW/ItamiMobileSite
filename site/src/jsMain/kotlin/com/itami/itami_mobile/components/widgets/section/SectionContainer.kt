700933f4bf1200bbc1a84206cca57e708674e818
Вадим <itamiomw@gmail.com>
2024-02-09 14:56:43 +0300

    1) Created SectionContainer widget for future sections

---
+++ site/src/jsMain/kotlin/com/itami/itami_mobile/components/widgets/section/SectionContainer.kt
@@ -0,0 +1,61 @@
+package com.itami.itami_mobile.components.widgets.section
+
+import androidx.compose.runtime.Composable
+import com.itami.itami_mobile.utils.Section
+import com.varabyte.kobweb.compose.foundation.layout.Column
+import com.varabyte.kobweb.compose.foundation.layout.ColumnScope
+import com.varabyte.kobweb.compose.ui.Alignment
+import com.varabyte.kobweb.compose.ui.Modifier
+import com.varabyte.kobweb.compose.ui.modifiers.*
+import com.varabyte.kobweb.silk.components.style.ComponentStyle
+import com.varabyte.kobweb.silk.components.style.ComponentVariant
+import com.varabyte.kobweb.silk.components.style.breakpoint.Breakpoint
+import com.varabyte.kobweb.silk.components.style.toModifier
+import org.jetbrains.compose.web.css.percent
+import org.jetbrains.compose.web.css.px
+
+val SectionContainerStyle by ComponentStyle {
+    base {
+        Modifier
+            .width(100.percent)
+            .height(100.percent)
+            .classNames("section-container")
+    }
+    Breakpoint.ZERO {
+        Modifier
+            .height(50.percent)
+            .padding(leftRight = 16.px)
+    }
+    Breakpoint.SM {
+        Modifier
+            .height(50.percent)
+            .padding(leftRight = 28.px)
+    }
+    Breakpoint.MD {
+        Modifier
+            .height(85.percent)
+            .padding(leftRight = 48.px)
+    }
+    Breakpoint.LG {
+        Modifier
+            .padding(leftRight = 94.px)
+    }
+    Breakpoint.XL {
+        Modifier.padding(leftRight = 134.px)
+    }
+}
+
+@Composable
+fun SectionContainer(
+    section: Section,
+    modifier: Modifier = Modifier,
+    horizontalAlignment: Alignment.Horizontal = Alignment.CenterHorizontally,
+    variant: ComponentVariant? = null,
+    content: @Composable ColumnScope.() -> Unit = {}
+) {
+    Column(
+        modifier = SectionContainerStyle.toModifier(variant).then(modifier).id(section.id),
+        horizontalAlignment = horizontalAlignment,
+        content = content
+    )
+}
