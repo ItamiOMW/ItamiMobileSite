beb7c9812389d34eb8e92a4409885ca9bdf4bb11
Вадим <itamiomw@gmail.com>
2024-02-27 17:43:23 +0300

    1) Added GradientCircle background to AboutSection;

--- site/src/jsMain/kotlin/com/itami/itami_mobile/components/sections/about/AboutSection.kt
+++ site/src/jsMain/kotlin/com/itami/itami_mobile/components/sections/about/AboutSection.kt
@@ -4,15 +4,20 @@ import androidx.compose.runtime.Composable
 import com.itami.itami_mobile.components.widgets.section.SectionContainer
 import com.itami.itami_mobile.components.widgets.section.SectionContainerStyle
 import com.itami.itami_mobile.components.widgets.skill.SkillItem
+import com.itami.itami_mobile.theme.brand
 import com.itami.itami_mobile.theme.fonts.*
 import com.itami.itami_mobile.utils.Res
 import com.itami.itami_mobile.utils.Section
 import com.itami.itami_mobile.utils.Skill
+import com.varabyte.kobweb.compose.css.CSSPosition
 import com.varabyte.kobweb.compose.css.TextAlign
+import com.varabyte.kobweb.compose.css.functions.RadialGradient
+import com.varabyte.kobweb.compose.css.functions.radialGradient
 import com.varabyte.kobweb.compose.foundation.layout.Arrangement
 import com.varabyte.kobweb.compose.foundation.layout.Column
 import com.varabyte.kobweb.compose.ui.Alignment
 import com.varabyte.kobweb.compose.ui.Modifier
+import com.varabyte.kobweb.compose.ui.graphics.Colors
 import com.varabyte.kobweb.compose.ui.modifiers.*
 import com.varabyte.kobweb.silk.components.layout.SimpleGrid
 import com.varabyte.kobweb.silk.components.layout.numColumns
@@ -21,13 +26,22 @@ import com.varabyte.kobweb.silk.components.style.breakpoint.Breakpoint
 import com.varabyte.kobweb.silk.components.style.toModifier
 import com.varabyte.kobweb.silk.components.text.SpanText
 import com.varabyte.kobweb.silk.theme.breakpoint.rememberBreakpoint
+import com.varabyte.kobweb.silk.theme.colors.palette.toPalette
 import org.jetbrains.compose.web.css.cssRem
 import org.jetbrains.compose.web.css.percent
 import org.jetbrains.compose.web.css.vh
 
 val AboutSectionVariant by SectionContainerStyle.addVariant {
+    val colorPalette = colorMode.toPalette()
     base {
-        Modifier.minHeight(100.vh)
+        Modifier
+            .minHeight(100.vh)
+            .backgroundImage(
+                radialGradient(RadialGradient.Shape.Circle, CSSPosition(x = 50.percent, y = 50.percent)) {
+                    add(colorPalette.brand.primary.toRgb().copyf(alpha = if (colorMode.isDark) 0.1f else 0.2f))
+                    add(Colors.Transparent, 63.percent)
+                }
+            )
     }
     Breakpoint.ZERO {
         Modifier.maxWidth(100.percent)
@@ -72,6 +86,7 @@ fun AboutSection() {
         }
     }
 }
+
 
 @Composable
 private fun TextContent() {
