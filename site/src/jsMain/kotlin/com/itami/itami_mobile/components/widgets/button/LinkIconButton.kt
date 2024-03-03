33f00a5499d8d067f75284df46a8566cdad393a0
Вадим <itamiomw@gmail.com>
2024-03-03 18:01:12 +0300

    1) Moved LinkIconButton to separate file

---
+++ site/src/jsMain/kotlin/com/itami/itami_mobile/components/widgets/button/LinkIconButton.kt
@@ -0,0 +1,81 @@
+package com.itami.itami_mobile.components.widgets.button
+
+import androidx.compose.runtime.Composable
+import com.itami.itami_mobile.theme.icons.IconStyle
+import com.varabyte.kobweb.browser.dom.ElementTarget
+import com.varabyte.kobweb.compose.ui.Modifier
+import com.varabyte.kobweb.compose.ui.modifiers.onClick
+import com.varabyte.kobweb.compose.ui.modifiers.scale
+import com.varabyte.kobweb.navigation.OpenLinkStrategy
+import com.varabyte.kobweb.silk.components.graphics.Image
+import com.varabyte.kobweb.silk.components.navigation.Link
+import com.varabyte.kobweb.silk.components.navigation.LinkStyle
+import com.varabyte.kobweb.silk.components.overlay.Tooltip
+import com.varabyte.kobweb.silk.components.style.addVariant
+import com.varabyte.kobweb.silk.components.style.hover
+import com.varabyte.kobweb.silk.components.style.toModifier
+
+val LinkIconButtonVariant by LinkStyle.addVariant {
+    base {
+        Modifier
+    }
+    hover {
+        Modifier.scale(1.1)
+    }
+}
+
+@Composable
+fun LinkIconButton(
+    path: String,
+    modifier: Modifier = Modifier,
+    tooltipText: String? = null,
+    onClick: (() -> Unit)? = null,
+    icon: @Composable () -> Unit,
+    openLinkStrategy: OpenLinkStrategy = OpenLinkStrategy.IN_NEW_TAB
+) {
+    Link(
+        path = path,
+        openExternalLinksStrategy = openLinkStrategy,
+        openInternalLinksStrategy = openLinkStrategy,
+        variant = LinkIconButtonVariant,
+        modifier = modifier.onClick { onClick?.invoke() },
+    ) {
+        icon()
+        if (tooltipText != null) {
+            Tooltip(
+                target = ElementTarget.PreviousSibling,
+                text = tooltipText
+            )
+        }
+    }
+}
+
+@Composable
+fun LinkIconButton(
+    path: String,
+    iconRes: String,
+    modifier: Modifier = Modifier,
+    iconModifier: Modifier = IconStyle.toModifier(),
+    tooltipText: String? = null,
+    onClick: (() -> Unit)? = null,
+    openLinkStrategy: OpenLinkStrategy = OpenLinkStrategy.IN_NEW_TAB
+) {
+    Link(
+        path = path,
+        openExternalLinksStrategy = openLinkStrategy,
+        openInternalLinksStrategy = openLinkStrategy,
+        variant = LinkIconButtonVariant,
+        modifier = modifier.onClick { onClick?.invoke() },
+    ) {
+        Image(
+            modifier = iconModifier,
+            src = iconRes
+        )
+        if (tooltipText != null) {
+            Tooltip(
+                target = ElementTarget.PreviousSibling,
+                text = tooltipText
+            )
+        }
+    }
+}
