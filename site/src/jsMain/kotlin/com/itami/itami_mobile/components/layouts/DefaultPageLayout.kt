b0df14663e1139cc70ba1f36517ff3f6ed16055f
Вадим <itamiomw@gmail.com>
2024-03-05 12:00:39 +0300

    1) Removed unnecessary Box inside DefaultPageLayout

--- site/src/jsMain/kotlin/com/itami/itami_mobile/components/layouts/DefaultPageLayout.kt
+++ site/src/jsMain/kotlin/com/itami/itami_mobile/components/layouts/DefaultPageLayout.kt
@@ -4,14 +4,11 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import com.itami.itami_mobile.components.sections.footer.Footer
 import com.itami.itami_mobile.components.sections.nav_header.NavHeader
-import com.varabyte.kobweb.compose.foundation.layout.Box
 import com.varabyte.kobweb.compose.foundation.layout.Column
 import com.varabyte.kobweb.compose.ui.Alignment
 import com.varabyte.kobweb.compose.ui.Modifier
 import com.varabyte.kobweb.compose.ui.modifiers.fillMaxSize
-import com.varabyte.kobweb.compose.ui.modifiers.gridTemplateColumns
 import kotlinx.browser.document
-import org.jetbrains.compose.web.css.fr
 
 @Composable
 fun DefaultPageLayout(
@@ -22,21 +19,12 @@ fun DefaultPageLayout(
         document.title = "Itami Mobile - $title"
     }
 
-    Box(
-        modifier = Modifier
-            .fillMaxSize()
-            .gridTemplateColumns { size(1.fr); size(auto) },
-        contentAlignment = Alignment.TopCenter
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .align(Alignment.TopCenter),
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            NavHeader()
-            content()
-            Footer()
-        }
+        NavHeader()
+        content()
+        Footer()
     }
 }
